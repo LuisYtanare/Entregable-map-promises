d9a9a75540f14cce51aecb4c6eadc0baff2cda70
LuisYtanare <gricert07alexander@gmail.com>
2021-10-23 15:57:46 -0400

    map-promises

---
+++ tarea5mappromises-main/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+ import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+const mensaje = "Bienvenido al paraiso de las frutas secas, nosotros secamos con amor";
+function App() {
+  return (
+<div name="background" class="p-3 mb-2 bg-dark text-white">
+    <div className="App">
+ <NavBar/>
+
+
+     <ItemListContainer name={mensaje} />
+
+      <header className="App-header">
+      </header>
+          </div>
+      </div>
+  );
+}
+
+
+
+export default App;
